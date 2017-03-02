f5140dbe429309eabc57bb79d1c6221111f06458
andrecampopiano <brocoyou@hotmail.com>
2017-03-01 23:39:07 -0300

    inicio de projeto

---
+++ Usando Firebase/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Usando Firebase
+//
+//  Created by André Luís  Campopiano on 01/03/17.
+//  Copyright © 2017 André Luís  Campopiano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
