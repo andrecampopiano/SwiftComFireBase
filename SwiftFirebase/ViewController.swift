631b55219d7cb79937f8a18c483d3eb8bb3525dd
andrecampopiano <brocoyou@hotmail.com>
2017-03-03 11:44:40 -0300

    conceitos básicos do firebase

--- SwiftFirebase/ViewController.swift
+++ SwiftFirebase/ViewController.swift
@@ -10,44 +10,79 @@ import UIKit
 import Firebase
 
 class ViewController: UIViewController {
-
+    
     let firebase = FIRDatabase.database().reference()
     
     @IBOutlet weak var lblText: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        FIRAuth.auth()?.createUser(withEmail: <#T##String#>, password: <#T##String#>, completion: <#T##FIRAuthResultCallback?##FIRAuthResultCallback?##(FIRUser?, Error?) -> Void#>)
-        
-        
-        
+        //Deslogar usuario
         
         /*
-        // Do any additional setup after loading the view, typically from a nib.
+        do{
+            try FIRAuth.auth()?.signOut()
+        }catch let erro as NSError{
+            print(erro.description)
+        }
+        */
         
-        let pontuacao = firebase.child("pontuacao")
-        
-        
-        //Remover item
-        //pontuacao.removeValue()
-        
-        
-        //Recuperar dados do Firebase
-        pontuacao.observe(FIRDataEventType.value, with: {(dados)in
-           let ponto = dados.value as! String
-            self.lblText.text = ponto
-        
+        FIRAuth.auth()?.signIn(withEmail: "brocoyou@icloud.com", password: "866886", completion: { (usuario, erro) in
+            if erro == nil {
+                print("Sucesso ao Logar usuario")
+            }else {
+                print("Erro ao logar: \(erro?.localizedDescription)")
+            }
         })
         
-    */
+        
+        
+        //Verifica usuario logado
+        FIRAuth.auth()?.addStateDidChangeListener({ (FIRAuth, usuario) in
+            if let usuarioLogado = usuario{
+                print("Usuario esta logado!! " + String(describing: usuarioLogado.email))
+            }else{
+                print("Usuario nao esta logado")
+            }
+        })
+        
+        
+        //        FIRAuth.auth()?.createUser(withEmail: "brocoyou@icloud.com", password: "866886", completion: { (usuario, erro) in
+        //
+        //            if erro == nil {
+        //                print("Sucesso ao cadastrar usuario")
+        //            }else {
+        //                print("Erro ao cadastrar usuario: \(erro?.localizedDescription)")
+        //            }
+        //        })
+        //
+        //
+        
+        
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        //        let pontuacao = firebase.child("pontuacao")
+        //
+        //
+        //        //Remover item
+        //        //pontuacao.removeValue()
+        //
+        //
+        //        //Recuperar dados do Firebase
+        //        pontuacao.observe(FIRDataEventType.value, with: {(dados)in
+        //           let ponto = dados.value as! String
+        //            self.lblText.text = ponto
+        //
+        //        })
+        
+        
         
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
